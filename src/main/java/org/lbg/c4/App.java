2c17f24ff89c347a55deeccf7d186e019f8fad81
kalkashi <kalkashi@kalkashi.com>
2024-10-24 11:17:31 +0100

    add changes

--- src/main/java/org/lbg/c4/App.java
+++ src/main/java/org/lbg/c4/App.java
@@ -1,6 +1,6 @@
 
 //import org.lbg.unreachable.ClassB;
-
+package org.lbg.c4;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
@@ -12,20 +12,27 @@ public class App {
     public static void main(String[] args) throws IOException {
         
 
-    int counter = 0;
+//    int counter = 0;
+//
+//    InputStreamReader isr = new InputStreamReader(System.in);
+//    LineNumberReader lnr = new LineNumberReader(isr);
+//
+//        try {
+//            while (lnr.readLine() != null)
+//                ;
+//            System.out.println("Num of lines: " + lnr.getLineNumber());
+//        } catch (IOException e) {
+//            System.out.println(e);
+//        }
 
-    InputStreamReader isr = new InputStreamReader(System.in);
-    LineNumberReader lnr = new LineNumberReader(isr);
-
-        try {
-            while (lnr.readLine() != null)
-                ;
-            System.out.println("Num of lines: " + lnr.getLineNumber());
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+        System.out.println("ADD RESULT   " + sum((int)4.3,5));
 
 
+    }
+
+
+    public static int sum(int x,int y){
+        return x+y;
     }
 }
 
