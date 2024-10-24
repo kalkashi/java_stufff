7d711b64248cb1b0761cdf81623259fd6dca2985
kalkashi <kalkashi@kalkashi.com>
2024-10-24 10:59:31 +0100

    new changes

--- src/main/java/org/lbg/c4/App.java
+++ src/main/java/org/lbg/c4/App.java
@@ -1,17 +1,1 @@
-package org.lbg.c4;
 
-import org.lbg.unreachable.ClassB;
-
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-        int x = 908;
-        System.out.println("Hellooooo !" + x);
-        ClassA ca = new ClassA();
-        ClassB cb = new ClassB();
-    }
-}
